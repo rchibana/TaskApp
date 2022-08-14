7d1c0c7e854093dd109306600afc62ae56119150
rchibana <rodrigo.chibana@gmail.com>
2022-08-14 11:25:20 -0300

    Initial commit

---
+++ src/main/kotlin/com/chibana/taskapp/TaskAppApplication.kt
@@ -0,0 +1,11 @@
+package com.chibana.taskapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaskAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/chibana/taskapp/TaskAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chibana.taskapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
