b901b2fd2802b141db37bb76bcabc8850d31f85f
rchibana <rodrigo.chibana@gmail.com>
2022-08-14 11:42:35 -0300

    Create body for service methods

--- src/main/kotlin/com/chibana/taskapp/repository/TaskRepository.kt
+++ src/main/kotlin/com/chibana/taskapp/repository/TaskRepository.kt
@@ -1,8 +1,13 @@
 package com.chibana.taskapp.repository
 
+import com.chibana.taskapp.model.StatusEnum
 import com.chibana.taskapp.model.TaskEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TaskRepository: CrudRepository<TaskEntity, Long>
+interface TaskRepository: CrudRepository<TaskEntity, Long> {
+
+    fun findAllByUsernameAndStatusIn(username: String, statusList: List<StatusEnum>): List<TaskEntity>
+
+}
--- src/main/kotlin/com/chibana/taskapp/service/TaskServiceImpl.kt
+++ src/main/kotlin/com/chibana/taskapp/service/TaskServiceImpl.kt
@@ -2,19 +2,23 @@ package com.chibana.taskapp.service
 
 import com.chibana.taskapp.model.StatusEnum
 import com.chibana.taskapp.model.TaskEntity
+import com.chibana.taskapp.repository.TaskRepository
 import org.springframework.stereotype.Service
 
 @Service
-class TaskServiceImpl : TaskService {
+class TaskServiceImpl(
+    private val taskRepository: TaskRepository
+) : TaskService {
+
     override fun createTask(taskEntity: TaskEntity): TaskEntity {
-        TODO("Not yet implemented")
+        return taskRepository.save(taskEntity)
     }
 
     override fun getTasksByUsername(username: String, statusEnum: StatusEnum): List<TaskEntity> {
-        TODO("Not yet implemented")
+        return taskRepository.findAllByUsernameAndStatusIn(username, listOf(statusEnum))
     }
 
     override fun getTasksByUsername(username: String, statusEnumList: List<StatusEnum>): List<TaskEntity> {
-        TODO("Not yet implemented")
+        return taskRepository.findAllByUsernameAndStatusIn(username, statusEnumList)
     }
 }
